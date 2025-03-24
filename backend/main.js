a6ac72ef582962f45aed92efbc2172772e18de49
Namfon2233 <ninlakarn.p@gmail.com>
2025-03-24 19:06:40 +0700

    auto-commit

--- backend/main.js
+++ backend/main.js
@@ -5,12 +5,15 @@ const app = express();
 
 const linkRoutes = require('./routes/linkRoutes');
 const groupRoutes = require('./routes/groupRoutes'); // ✅ เพิ่ม
+const logRoutes = require('./routes/logRoutes'); // ✅ เพิ่ม logRoutes ใหม่
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/api/links', linkRoutes);
+app.use('/api/logs', logRoutes); // ✅ เพิ่ม Endpoint /api/logs
 app.use('/', groupRoutes); // ✅ ใช้งาน groupRoutes
 
 app.get('/', (req, res) => {
